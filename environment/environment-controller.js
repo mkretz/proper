51867d3bd49d3fec735a7373780ccfa45501a212
Mathis Kretz <mathis.kretz@gmail.com>
2016-06-18 23:27:41 +0200

    finished crud for environments

--- environment/environment-controller.js
+++ environment/environment-controller.js
@@ -1,6 +1,6 @@
 function EnvironmentController(server, ProjectController, Environment) {
     var utils = require('../utils')(server);
-
+    var _ = require('lodash');
     return {
         getProject: function (req, res, next) {
             ProjectController.loadProject(req)
@@ -19,16 +19,56 @@ function EnvironmentController(server, ProjectController, Environment) {
                 })
         },
         getEnvironment: function (req, res) {
+            utils.getRequestData(req, 'project').getEnvironments({ where: {id: req.params.environmentid } })
+                .then(function (environment) {
+                    if (environment.length > 0) {
+                        res.send(environment[0]);
+                    }
+                    else {
+                        res.send(404);
+                    }
 
+                })
         },
         createEnvironment: function (req, res) {
-
+            utils.getRequestData(req, 'project').createEnvironment(req.body)
+                .then(function (environment) {
+                    var pathParams = {projectid: environment.projectId, environmentid: environment.id};
+                    res.header('Location', server.router.render('getenvironment', pathParams));
+                    res.send(204);
+                });
         },
         updateEnvironment: function (req, res) {
-
+            utils.getRequestData(req, 'project').getEnvironments({ where: {id: req.params.environmentid } })
+                .then(function (environment) {
+                    if (environment.length > 0) {
+                        Environment.update(req.body, {
+                            where: {
+                                id: environment[0].id
+                            }
+                            })
+                            .then(function () {
+                                res.send(204);
+                            });
+                    }
+                    else {
+                        res.send(404);
+                    }
+                });
         },
         deleteEnvironment: function (req, res) {
-
+            utils.getRequestData(req, 'project').getEnvironments({ where: {id: req.params.environmentid } })
+                .then(function (environment) {
+                    if (environment.length > 0) {
+                        utils.getRequestData(req, 'project').removeEnvironment(environment)
+                            .then(function () {
+                                res.send(204);
+                            });
+                    }
+                    else {
+                        res.send(404);
+                    }
+                });
         }
     }
 
