3496d20f483ab2d61acd5bd2a969706b8a5a453a
Mathis Kretz <mathis.kretz@gmail.com>
2016-06-25 23:02:52 +0200

    added more detailed checks to tests

--- test/server-test.js
+++ test/server-test.js
@@ -1,6 +1,7 @@
 require('assert');
 var restify = require('restify');
 var server = require('../server.js');
+var _ = require('lodash');
 
 before(function (done) {
     var db = require('./test-database');
@@ -29,65 +30,117 @@ var client = restify.createJsonClient({
 });
 
 describe('the root endpoint', function() {
+    var root;
+
     it('should allow a GET', function (done) {
         client.get('/', function (err, req, res, data) {
-            checkGetResponse(err,req,res,data,'invalid response from /');
+            root = checkGetResponse(err,req,res,data,'invalid response from /');
             done();
         });
+    });
+
+    it('should link to projects', function (done) {
+        checkLinkPresent(root, 'projects', 'expected projects link to be present');
+        done();
     });
 });
 
 describe('the project list endpoint', function() {
+    var projectList;
     it('should allow a GET', function (done) {
         client.get('/project', function (err, req, res, data) {
-            checkGetResponse(err,req,res,data,'invalid response from /project');
+            projectList = checkGetResponse(err,req,res,data,'invalid response from /project');
             done();
         });
+    });
+
+    it('should return one project', function (done) {
+        checkArrayLength(projectList, 1);
+        done();
     });
 });
 
 describe('the project endpoint', function() {
+    var project;
     it('should allow a GET', function (done) {
         client.get('/project/1', function (err, req, res, data) {
-            checkGetResponse(err,req,res,data,'invalid response from /project');
+            project = checkGetResponse(err,req,res,data,'invalid response from /project');
             done();
         });
+    });
+
+    it('should link to versions, environments and itself', function (done) {
+        checkLinkPresent(project, 'versions', 'expected versions link to be present');
+        checkLinkPresent(project, 'environments', 'expected environments link to be present');
+        checkLinkPresent(project, 'self', 'expected self link to be present');
+        done();
     });
 });
 
 describe('the version list endpoint', function() {
+    var versionList;
     it('should allow a GET', function (done) {
         client.get('/project/1/version', function (err, req, res, data) {
-            checkGetResponse(err,req,res,data,'invalid response from /project');
+             versionList = checkGetResponse(err,req,res,data,'invalid response from /project');
+            checkArrayLength(versionList, 1);
             done();
         });
+    });
+
+    it('should return one version', function (done) {
+        checkArrayLength(versionList, 1);
+        done();
     });
 });
 
 describe('the version endpoint', function() {
+    var version;
     it('should allow a GET', function (done) {
         client.get('/project/1/version/1', function (err, req, res, data) {
-            checkGetResponse(err,req,res,data,'invalid response from /project');
+            version = checkGetResponse(err,req,res,data,'invalid response from /project');
             done();
         });
+    });
+
+    it('should link to project and itself', function (done) {
+        checkLinkPresent(version, 'self', 'expected self link to be present');
+        checkLinkPresent(version, 'project', 'expected project link to be present');
+        done();
     });
 });
 
 describe('the environment list endpoint', function() {
+    var envList;
     it('should allow a GET', function (done) {
         client.get('/project/1/environment', function (err, req, res, data) {
-            checkGetResponse(err,req,res,data,'invalid response from /project');
+            envList = checkGetResponse(err,req,res,data,'invalid response from /project');
+            checkArrayLength(envList, 1);
             done();
         });
     });
+
+    it('should return one environment', function (done) {
+        checkArrayLength(envList, 1);
+        done();
+    });
+
 });
 
 describe('the environment endpoint', function() {
+    var env;
     it('should allow a GET', function (done) {
         client.get('/project/1/environment/1', function (err, req, res, data) {
-            checkGetResponse(err,req,res,data,'invalid response from /project');
+            env = checkGetResponse(err,req,res,data,'invalid response from /project');
+            checkLinkPresent(env, 'self', 'expected self link to be present');
+            checkLinkPresent(env, 'project', 'expected project link to be present');
             done();
         });
+    });
+
+    it('should link to project and itself', function (done) {
+        checkLinkPresent(env, 'self', 'expected self link to be present');
+        checkLinkPresent(env, 'project', 'expected project link to be present');
+        done();
     });
 });
 
@@ -100,5 +153,29 @@ function checkGetResponse(err, req, res, data, errorMessage) {
             throw new Error(errorMessage);
         }
         return data;
+    }
+}
+
+function getLink(dataObject, relName) {
+    var result = undefined;
+    var links = dataObject._links;
+    if (!_.isUndefined(links)) {
+        var link = _.find(links, {rel : relName});
+        result = link.href;
+    }
+    return result;
+}
+
+function checkLinkPresent(dataObject, relName, errorMessage) {
+    var result  = getLink(dataObject,relName);
+    if (_.isUndefined(result)) {
+        throw new Error(errorMessage);
+    }
+    return result;
+}
+
+function checkArrayLength(array, expectedLength) {
+    if (array.length != expectedLength) {
+        throw new Error('expecting ' + expectedLength + ' elements, got ' + array.length);
     }
 }
