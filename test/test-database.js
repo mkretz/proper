a17ce7dfe06764dcb85766c2bfa940aec7a4b109
Mathis Kretz <mathis.kretz@gmail.com>
2016-06-25 23:48:38 +0200

    removed unused references to env variables in test setup

--- test/test-database.js
+++ test/test-database.js
@@ -1,11 +1,6 @@
 function connectToDatabase() {
     var Sequelize = require('sequelize');
 
-    // db connection env variables
-    var dbUser = process.env.PROPER_DB_USER;
-    var dbPwd = process.env.PROPER_DB_PWD;
-    var dbHost = process.env.PROPER_DB_HOST;
-    var dbPort = process.env.PROPER_DB_PORT;
     var sequelize = new Sequelize('sqlite://:memory:');
     return sequelize;
 }
