072848307820448a83a6f68e94c9e4eec83d1e3a
Mathis Kretz <mathis.kretz@gmail.com>
2016-06-27 08:48:10 +0200

    added artificial primary key attribute to deployment

--- deployment/deployment-model.js
+++ deployment/deployment-model.js
@@ -1,6 +1,11 @@
 module.exports = function (sequelize, Environment, Version) {
     var Sequelize = require('sequelize');
     var Deployment = sequelize.define('deployment', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
         status: {
             type: Sequelize.STRING
         },
